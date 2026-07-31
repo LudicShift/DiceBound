462161b20cc09f7b73e437b82833f2948d260094
khjj07@gmail.com <khjj07@gmail.com>
2026-08-01 02:38:04 +0900

    feat : 애니메이션 수정

--- Assets/YatchDungeon/Scripts/DIce/DiceWidget.cs
+++ Assets/YatchDungeon/Scripts/DIce/DiceWidget.cs
@@ -9,38 +9,45 @@ namespace YatchDungeon
 {
     public class DiceWidget : ImageWidget
     {
-        private Animator _animator;
+     
         private bool _isRolling;
         private int _number;
 
         public Func<int, Sprite> spriteGetter;
+        public Func<int, Sprite> animationSpriteGetter;
         private bool _isMoving;
-
-        public override void Awake()
-        {
-            base.Awake();
-            _animator = GetComponent<Animator>();
-        }
+        private int _animationIndex;
 
         public IEnumerator Roll(Action finishCallback)
         {
             _isRolling = true;
-            // _animator.SetInteger("Index",Random.Range(1,3));
+            //_animator.SetInteger("Index",Random.Range(1,3));
             //yield return new WaitUntil(() => !_isRolling);
-            yield return null;
+            var rollStartIndex = Random.Range(0, 5);
+            var rollEndIndex = rollStartIndex + 12;
+            _animationIndex = rollStartIndex;
+            yield return transform.DOMoveY(50f, 0.1f).SetRelative(true).WaitForCompletion();
+            yield return DOTween.To(GetRollIndex, SetRollIndex, rollEndIndex,0.3f).WaitForCompletion();
+            yield return transform.DOMoveY(-50f, 0.1f).SetRelative(true).WaitForCompletion();
             _number = Random.Range(1, 6);
-            finishCallback?.Invoke();
             SetSprite(spriteGetter.Invoke(_number));
+            finishCallback?.Invoke();
+        }
+
+        private void SetRollIndex(int value)
+        {
+            _animationIndex = value;
+            SetSprite(animationSpriteGetter.Invoke(_animationIndex));
+        }
+
+        private int GetRollIndex()
+        {
+            return _animationIndex;
         }
 
         public bool IsMoving()
         {
             return _isMoving;
-        }
-        public void RollEnd()
-        {
-            _isRolling = false;
-         
         }
 
         public int GetNumber()
@@ -51,10 +58,7 @@ namespace YatchDungeon
         public void MoveTo(Vector3 position)
         {
             _isMoving = true;
-            transform.DOMove(position,1).OnComplete(() =>
-            {
-                _isMoving = false;
-            });
+             transform.DOMove(position, 1).OnComplete(() => { _isMoving = false; });
         }
 
         public void Warp(Vector3 position)
